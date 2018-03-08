16f9bc39635c6fbc6340183a19919dcca676a35a
Jeetu singh <xtaraim@gmail.com>
2018-03-08 21:22:37 +0530

    Create mouse.py

---
+++ mouse.py
@@ -0,0 +1,29 @@
+>>> import pyautogui
+>>> pyautogui.size()
+(1366, 768)
+>>> width, height = pyautogui.size()
+>>> width
+1366
+>>> height
+768
+>>> pyautogui.position()
+(889, 249)
+>>> pyautogui.position()
+(882, 326)
+>>> pyautogui.position()
+(882, 326)
+>>> pyautogui.moveTo(10, 10)
+>>> pyautogui.moveTo(10, 10, duration=1.5)
+>>> pyautogui.moveTo(10, 10, duration=1.5)
+>>> pyautogui.moveTo(10, 10, duration=1.5)
+>>> pyautogui.moveTo(10, 10, duration=1.5)
+>>> pyautogui.moveRel(20, 0)
+>>> pyautogui.moveRel(200, 0)
+>>> pyautogui.moveRel(200, 0, duration=2)
+>>> pyautogui.movereal(0, -100)
+>>> pyautogui.moveRel(0, -100)
+>>> pyautogui.moveRel(0, -100, duration=1)
+>>> pyautogui.position()
+(877, 515)
+>>> pyautogui.click(339, 38)
+>>> pyautogui.click()
